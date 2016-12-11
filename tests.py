86dd678384170aa3d813e5ff542fec522254d740
tenfy <364755805@qq.com>
2016-12-11 16:48:27 +0800

    post feeds

--- tests.py
+++ tests.py
@@ -5,7 +5,8 @@ import unittest
 
 from config import basedir
 from app import app, db
-from app.models import User
+from app.models import User, Post
+from datetime import datetime, timedelta
 
 
 class TestCase(unittest.TestCase):
@@ -58,6 +59,55 @@ class TestCase(unittest.TestCase):
         self.assertFalse(u1.is_following(u2))
         self.assertEqual(u1.followed.count(), 0)
         self.assertEqual(u2.followers.count(), 0)
+
+    def test_follow_posts(self):
+        u1 = User(nickname='john', email='John@example.com')
+        u2 = User(nickname='susan', email='susan@except.com')
+        u3 = User(nickname='mary', email='mary@example.com')
+        u4 = User(nickname='david', email='david@example.com')
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.add(u3)
+        db.session.add(u4)
+        utcnow = datetime.utcnow()
+        p1 = Post(body='post from john', author=u1,
+                  timestamp=utcnow + timedelta(seconds=1))
+        p2 = Post(body='post from susan', author=u2,
+                  timestamp=utcnow + timedelta(seconds=2))
+        p3 = Post(body='post from mary', author=u3,
+                  timestamp=utcnow + timedelta(seconds=3))
+        p4 = Post(body='post from david', author=u4,
+                  timestamp=utcnow + timedelta(seconds=4))
+        db.session.add(p1)
+        db.session.add(p2)
+        db.session.add(p3)
+        db.session.add(p4)
+        db.session.commit()
+        u1.follow(u1)
+        u1.follow(u2)
+        u1.follow(u4)
+        u2.follow(u2)
+        u2.follow(u3)
+        u3.follow(u3)
+        u3.follow(u4)
+        u4.follow(u4)
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.add(u3)
+        db.session.add(u4)
+        db.session.commit()
+        f1 = u1.followed_posts().all()
+        f2 = u2.followed_posts().all()
+        f3 = u3.followed_posts().all()
+        f4 = u4.followed_posts().all()
+        self.assertEqual(len(f1), 3)
+        self.assertEqual(len(f2), 2)
+        self.assertEqual(len(f3), 2)
+        self.assertEqual(len(f4), 1)
+        self.assertEqual(f1, [p4, p2, p1])
+        self.assertEqual(f2, [p3, p2])
+        self.assertEqual(f3, [p4, p3])
+        self.assertEqual(f4, [p4])
 
 
 if __name__ == '__main__':
